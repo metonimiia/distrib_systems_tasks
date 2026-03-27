4c89a7bf1f0c8357a3c740a55ab1f7a6d0c2513e
ia332s14 <ia332s14@csc.sibsutis.ru>
2026-03-28 00:43:55 +0700

    lab7

--- weeks/week-07/starter/service.py
+++ weeks/week-07/starter/service.py
@@ -1,19 +1,40 @@
 import grpc
 from concurrent import futures
-# Импортируйте сгенерированные модули
-# import service_pb2
-# import service_pb2_grpc
 
-class ServiceImplementation: # Унаследуйтесь от сгенерированного Servicer
-    pass
-    # Реализуйте методы сервиса
+import service_pb2
+import service_pb2_grpc
+
+
+class LogsServiceImplementation(service_pb2_grpc.LogsServiceServicer):
+
+    def CreateLog(self, request, context):
+        log_text = request.log
+        level = request.level
+
+        print("Received log:")
+        print("log =", log_text)
+        print("level =", level)
+
+        return service_pb2.CreateLogResponse(
+            message=f"Log '{log_text}' with level '{level}' created"
+        )
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # service_pb2_grpc.add_MyServiceServicer_to_server(ServiceImplementation(), server)
+
+    service_pb2_grpc.add_LogsServiceServicer_to_server(
+        LogsServiceImplementation(),
+        server
+    )
+
     server.add_insecure_port('[::]:50051')
+
     server.start()
+    print("gRPC server started on port 50051")
+
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
